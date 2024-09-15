bc818154b42e9581f604c21f6925cdac9cf5cad4
William Mansfield <william.mansfield@mymail.champlain.edu>
2024-09-15 11:24:32 -0400

    addition of other requirements for the assignment 3

--- HomeWork/Assignment3/First Project/code.asm
+++ HomeWork/Assignment3/First Project/code.asm
@@ -6,22 +6,25 @@ extrn ExitProcess : proc ; This funciton is important its like return 0 function
 .data ; setions inzalized and uninizalized 
 	myString BYTE "The awnser is: ",0 ; we create inzialided data of size BYTE that contains a string 
 									  ;we a ,0 to Signify the end
+	D DWORD 1 ;create a dword with the value 1 -> 4 bytes
+	C DWORD 2 ;create a dword with the value 2 -> 4 bytes
+	B DWORD 3 ;create a dword with the value 3 -> 4 bytes
+	A DWORD 4 ;create a dword with the value 4 -> 4 bytes
+
 
 .code ;sections exicutable code / instructions 
 _main PROC ; start of the main procedure also indicates the entery point for the porgram
 
-mov rax , 1 ;moves the decimal 1 into the rax register
-mov rbx , 2 ;moves the decunal 2 into the rbx register
-add rax, rbx; adds the rbx and rax regisetrs and places it in rax
+mov eax , A ;moves the DWORD A into the eax register
+mov ebx , B ;moves the DWORD B into the ebx register
+add eax, ebx; adds the eax and ebx regisetrs and places it in eax
 
 ;define part two of the equation
-mov rcx, 3 ;move the decimal 4 into the rcx register
-mov rdx, 4 ;move the decimal 4 into the rdx register
-add rcx, rdx ;add rdx to rcx sum is stored in the rcx register
+mov ecx, C ;move the DWORD C into the ecx register
+mov edx, D ;move the DWORD D into the edx register
+add ecx, edx ;add ecx to edx sum is stored in the rcx register
 
-sub rax, rcx ;subract the rcx regisert from the rax register and store in the rax register 
-;(will return a negitive number)
-
+sub eax, ecx ;subract the ecx regisert from the eax register and store in the eax register 
 
 xor rcx, rcx ; 0s out the rcx register
 call ExitProcess ; calls the exitProcess procedure and releases resources
