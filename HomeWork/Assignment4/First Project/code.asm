e8dafd6ec32bff237d4bfa4ceca80a51ab8e5223
William Mansfield <william.mansfield@mymail.champlain.edu>
2024-09-24 12:21:27 -0400

    Completion of assignment 4 (comments need to be reviewed before turn in)

--- HomeWork/Assignment4/First Project/code.asm
+++ HomeWork/Assignment4/First Project/code.asm
@@ -13,33 +13,40 @@ extrn ExitProcess : proc ; This funciton is important its like return 0 function
 _main PROC ; start of the main procedure also indicates the entery point for the porgram
 sub rsp, 28h ;sub tracnt 28h from rsp (this is for the debugger)
 
+LEA rsi, wArr ;load effective memeory address of wArr and place it in rsi
+LEA rdi, dArr ;load effective memeory address of dArr and place it in rdi
+
+;first multiplacation
 mov ax, wArr ;move the first position of wArr into ax
 mov dx, 8 ;move 8 into dx
 
 mul dx ;multiply ax and dx
-;store in dArr loc
+movzx ebx, ax ;store the 16 bit val in the 32 bit register using movzx (does not carry sign)
+mov dArr, ebx ;store the val of ebx in the first pos of dArr 
 
-LEA rsi, wArr; store the mem address of wArr
-mov ax, WORD PTR [rsi + 1 * 2] ;take the mem address and add the offset pow(2) then move it to ax
+;second multiplacation
+mov ax, WORD PTR [rsi + 1 * 2] ;take the mem address and add the index * size and put it into ax
 mov dx, 8 ; move 8 into dx
 
 mul dx;multiply ax and dx
-;store in dArr loc
+movzx ebx, ax ;store the 16 bit val in the 32 bit register using movzx (does not carry sign)
+mov [rdi + 1 * 4], DWORD PTR ebx ;store the val of ebx in the correct index pos of dArr (locaiton + index * size)
 
-LEA rsi, wArr; store the mem address of wArr
-mov ax, [rsi + 4] ;take the mem address and add the offset pow(2) then move it to ax
-mov dx, 8 ; move 8 into dx
-
-mul dx;multiply ax and dx
-;store in dArr loc
-
-LEA rsi, wArr; store the mem address of wArr
-mov ax, [rsi + 16] ;take the mem address and add the offset pow(2) then move it to ax
+;third multiplcation
+mov ax, WORD PTR [rsi + 2 * 2] ;take the mem address and add the index * size and put it into ax
 mov dx, 8 ; move 8 into dx
 
 mul dx ;multiply ax and dx
+movzx ebx, ax ;store the 16 bit val in the 32 bit register using movzx (does not carry sign)
+mov [rdi + 2 * 4], ebx ;store the val of ebx in the correct index pos of dArr (locaiton + index * size)
 
+;fourth mutliplcation
+mov ax, [rsi + 3 * 2] ;take the mem address and add the index * size and put it into ax
+mov dx, 8 ; move 8 into dx
 
+mul dx ;multiply ax and dx
+movzx ebx, ax;store the 16 bit val in the 32 bit register using movzx (does not carry sign)
+mov [rdi + 3 * 4], ebx ;store the val of ebx in the correct index pos of dArr (locaiton + index * size)
 
 
 xor rcx, rcx ; 0s out the rcx register
