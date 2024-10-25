7af8c176cbacd7b36d2a8d095ba201db884284f5
William Mansfield <william.mansfield@mymail.champlain.edu>
2024-10-25 15:24:09 -0400

    removing of inf fixing becasue it does not work

--- HomeWork/Assignment8/First Project/code.asm
+++ HomeWork/Assignment8/First Project/code.asm
@@ -9,48 +9,107 @@ Vector2f STRUCT
 Vector2f ENDS
 .data 
 align 16
-testVec Vector2f <4.5, 6.6>
+testVecArray REAL4 4.5, 6.6
 
-
+;main start
 .code 
 _main PROC 
-sub rsp, 28h
+	sub rsp, 28h
 
-movss xmm1, testVec.x
-movss xmm0, testVec.y
+	lea rax, testVecArray
 
-mov rcx, 2
+	vmovaps xmm1, XMMWORD PTR [rax]
 
-call _Pow
-movss testVec.x, xmm0
+	mov rcx, 2
 
+	call _PowPacked
 
+	;move reslut back into xmm1 for additon
+	vmovaps xmm1, xmm0
+	;clear zmm0
+	XORPS xmm0, xmm0
+	XORPS xmm2, xmm2
 
-xor rcx, rcx ; clears out the rcx register
+	call _GenSqrt
+
+	;zero it out to the correct size of the vector
+	andps xmm0, xmm1
+	
+	divps xmm0, xmm1
+
+	;fix inf erros
+	;orps xmm0, xmm1
+	
+
+	;clear xmm1
+	xorps xmm1, xmm1	
+
+	xor rcx, rcx ; clears out the rcx register
 call ExitProcess
 _main ENDP
 
 ;takes two things the number xmm1 and the exponet rcx
 _Pow PROC
-push rbp
-sub rsp, 20h
-lea rbp, [rsp + 20h]
+	push rbp
+	sub rsp, 20h
+	lea rbp, [rsp + 20h]
 
-dec rcx ;make it a 0-x range
+	dec rcx ;make it a 0-x range
 
-;save of the value
-movss xmm0, xmm1
+	;save of the value
+	movss xmm0, xmm1
 
-myLoop:
-mulps xmm0, xmm1
-loop myLoop
 
-lea rsp, [rbp]
-pop rbp
-ret
+	myLoop:
+		mulps xmm0, xmm1
+	loop myLoop
+
+	lea rsp, [rbp]
+	pop rbp
+	ret
 _Pow ENDP
 
-END
+;takes rcx counter and xmm1 as the operands
+_PowPacked PROC
+	push rbp
+	sub rsp, 20h
+	lea rbp, [rsp + 20h]
 
+	sub rcx, 1
 
+	;save the values
+	vmovaps xmm0, xmm1
 
+	powLoop:
+		MULPS xmm0, xmm1
+	loop powLoop
+	
+	lea rsp, [rbp]
+	pop rbp
+	ret
+_PowPacked ENDP
+
+_GenSqrt PROC
+	push rbp
+	sub rsp, 20h
+	lea rbp, [rsp + 20h]
+
+	;horizontal addition using AVX so i can palce the result in the first postion
+	VHADDPS xmm0 ,xmm1, xmm0
+	;spoof xmm2 for formating and getting one number
+	VHADDPS xmm0, xmm0, xmm2
+
+	;sqr root
+	sqrtps xmm0, xmm0
+
+	;creates a packed data full of the divisor
+	haddps xmm0, xmm0
+	haddps xmm0, xmm0
+
+	
+
+	lea rsp, [rbp]
+	pop rbp
+	ret
+_GenSqrt ENDP
+ END
