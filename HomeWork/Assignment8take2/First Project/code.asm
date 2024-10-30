f1646df8dfa34ed327d9883c2351e5bdafbb73c9
William Mansfield <william.mansfield@mymail.champlain.edu>
2024-10-30 10:23:03 -0400

    addition of take 2 of the vectors

--- HomeWork/Assignment8take2/First Project/code.asm
+++ HomeWork/Assignment8take2/First Project/code.asm
@@ -9,18 +9,27 @@ Vector2f STRUCT
 Vector2f ENDS
 .data 
 align 16
-testVecArray REAL4 4.5, 6.6
+vecArray2D REAL4 4.5, 6.6
+align 16
+vecArray3D REAL4 4.5, 6.6, 3.7
 
-wallVec REAL4 2.2, 3.3
-;givin some up vector (already normalized)
-;wallNorm REAL4 1.0, 0.0
+align 16
+vecWall REAL4 1.0,4.8
+
+align 16
+;some up vec
+vecNor REAL4 1.0,0.0
+
+align 16
+;const 
+constant REAL4 -2.0
 
 ;main start
 .code 
 _main PROC 
 	sub rsp, 28h
 
-	lea rax, testVecArray
+	lea rax, vecArray2D
 
 	vmovaps xmm0, XMMWORD PTR [rax]
 	vmovaps xmm1, xmm0
@@ -44,21 +53,68 @@ _main PROC
 	vdivps xmm0, xmm1, xmm0
 	;Noramilized!!
 
-	;relfection vector
-	lea rax, wallVec
+	;2d relfection
 
-	;some how geting a one 
-	XORPS xmm2, xmm2
-	vmovups xmm2, XMMWORD PTR [rax]
+	lea rax, vecWall
 
-	;lea rax, wallNorm
+	vmovaps xmm3, XMMWORD PTR [rax]
 
-	;vmovups xmm3, XMMWORD PTR [rax]
+	lea rax, vecNor
 
-	;d - 2(d * n)n
+	vmovaps xmm4, XMMWORD PTR [rax]
+	
 
-	vmulps xmm4, xmm2, xmm3
+	;d dot n (unnormalized d which is in xmm1)
+	vmulps xmm5, xmm1, xmm4
 
+	haddps xmm5, xmm5
+	shufps xmm5,xmm5, 8
+
+	lea rax, constant
+	;-2(dn)
+	vmovaps xmm6, XMMWORD PTR [rax]
+	shufps xmm6,xmm6,128
+
+	;mul 
+
+	mulps xmm6, xmm5
+
+
+	;2dn *n
+
+	mulps xmm6, xmm4
+
+	;add d -2dnn
+
+	;reflection in xmm7
+	vaddps xmm7, xmm0, xmm6
+
+	;3d array
+	lea rax, vecArray3D
+
+	vmovaps xmm0, XMMWORD PTR [rax]
+	vmovaps xmm1, xmm0
+	
+	;square
+
+	mulps xmm0, xmm0
+
+	;add 
+
+	haddps xmm0,xmm0
+	haddps xmm0,xmm0
+
+	shufps xmm0,xmm0, 8
+	;root
+
+	vsqrtps xmm0, xmm0
+	
+
+	;div
+	vdivps xmm0, xmm1, xmm0
+	;Noramilized!!
+
+	
 
 	xor rcx, rcx ; clears out the rcx register
 call ExitProcess
